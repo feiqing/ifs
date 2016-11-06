947d63b71f6eb88f8e90b84b5848893f496f7731
feiqing <zhujifang666@163.com>
2016-11-06 14:14:22 +0800

    更新publish.sh

--- publish.sh
+++ publish.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+
+tomcat_dir=/usr/local/apache-tomcat-8.5.6
+app_name=ifs
+
 ## 判断是否执行成功
 check_result() {
     if  [ "$1" != "0" ] ; then
+        echo "execute error"
         exit -1
     fi
 }
 
 ## 拉取最新代码
-git pull
+# git pull
 
 check_result $?
 
@@ -19,19 +24,18 @@ mvn package -Dmaven.test.skip=true
 check_result $?
 
 ## 拷贝war包到目标目录
-rm -rf /usr/local/apache-tomcat-8.5.6/webapps/IFS*
-mv ./target/IFS.war /usr/local/apache-tomcat-8.5.6/webapps/
+rm -rf ${tomcat_dir}/webapps/${app_name}*
+mv ./target/${app_name}.war ${tomcat_dir}/webapps/
 
 check_result $?
 
 start_time=`date +%s`
 
 ## 重启tomcat
-cd /usr/local/apache-tomcat-8.5.6/bin/
-echo "--> shutdown.sh"
-./shutdown.sh
-echo "--> startup.sh"
-./startup.sh
+echo "--> ${tomcat_dir} shutdown.sh"
+${tomcat_dir}/bin/shutdown.sh
+echo "--> ${tomcat_dir} startup.sh"
+${tomcat_dir}/bin/startup.sh
 
 end_time=`date +%s`
 sum=$(( $end_time-$start_time ))
