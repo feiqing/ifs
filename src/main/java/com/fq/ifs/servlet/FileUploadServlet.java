4ac0b6e8d08c787e343b2f380448329d0bca49d7
feiqing <zhujifang666@163.com>
2016-11-06 15:31:42 +0800

    更新publish.sh

--- src/main/java/com/fq/ifs/servlet/FileUploadServlet.java
+++ src/main/java/com/fq/ifs/servlet/FileUploadServlet.java
@@ -98,8 +98,8 @@ public class FileUploadServlet extends HttpServlet {
         File dir = new File(saveDir);
         if (!dir.exists()) {
             dir.mkdirs();
-            dir.setReadable(true);
-            dir.setWritable(true);
+            dir.setReadable(true, false);
+            dir.setWritable(true, false);
         }
     }
 }
