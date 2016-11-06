689250d3da2aa8b5cc678bd289ee780f4089bcf1
feiqing <zhujifang666@163.com>
2016-11-06 16:49:28 +0800

    更新publish.sh

--- src/main/java/com/fq/ifs/servlet/FileUploadServlet.java
+++ src/main/java/com/fq/ifs/servlet/FileUploadServlet.java
@@ -1,6 +1,6 @@
 package com.fq.ifs.servlet;
 
-import com.fq.ifs.servlet.dao.FileDAO;
+import com.fq.ifs.servlet.dao.IfsDAO;
 import com.google.common.base.Strings;
 import com.google.common.io.ByteStreams;
 
@@ -21,42 +21,46 @@ import java.util.UUID;
 @WebServlet(name = "FileUploadServlet", urlPatterns = "/upload.action", asyncSupported = true)
 public class FileUploadServlet extends HttpServlet {
 
-    private FileDAO dao = new FileDAO();
+    private IfsDAO dao = new IfsDAO();
 
-    private static final String ROOT_DIR = "/data/files/";
+    private static final String FILE_ROOT_DIR = "/files/";
 
-    private static final String LINK_ROOT_DIR = "http://139.129.9.166:8000/files/";
+    private static final String LINK_ROOT_DIR = "http://139.129.9.166:5525/files/";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        Part image = request.getPart("file");
-        String fileName = getFileName(image);
-        if (isFileValid(image, fileName)) {
-            String uniqueFileName = toUniqueName(fileName);
+
+        Part file = request.getPart("file");
+        String fileName = getFileName(file);
+        if (isValidFile(file, fileName)) {
+            String uniqueName = toUniqueName(fileName);
 
             // 保存文件
-            String levelDir = toLevelDir(uniqueFileName);
-            String saveDir = ROOT_DIR + levelDir;
+            String levelDir = toLevelDir(uniqueName);
+            String saveDir = FILE_ROOT_DIR + levelDir;
             makeDirs(saveDir);
-            saveFile(saveDir + uniqueFileName, image.getInputStream());
+            if (saveFile(saveDir + uniqueName, file.getInputStream())) {
+                // 生成外链
+                String url = LINK_ROOT_DIR + levelDir + uniqueName;
+                dao.insertFile(fileName, url, file.getSize());
 
-            // 生成外链
-            String url = LINK_ROOT_DIR + levelDir + uniqueFileName;
-            dao.insertFile(fileName, url);
-
-            response.getWriter().println(toLinkUrl(url));
-        } else {
-            response.getWriter().println("wrong file");
+                response.getWriter().println(toLinkUrl(url));
+                return;
+            }
         }
+
+        response.getWriter().println("error");
     }
 
-    private void saveFile(String filePath, InputStream is) throws IOException {
+    private boolean saveFile(String filePath, InputStream is) throws IOException {
         File file = new File(filePath);
-        file.createNewFile();
-        makeVisitable(file);
-        FileOutputStream fos = new FileOutputStream(file);
-        ByteStreams.copy(is, fos);
+        if (file.createNewFile()) {
+            setVisitable(file);
+            long copy = ByteStreams.copy(is, new FileOutputStream(file));
+            return copy != 0;
+        }
+        return false;
     }
 
     private String toLinkUrl(String url) {
@@ -64,7 +68,7 @@ public class FileUploadServlet extends HttpServlet {
         return String.format(linkPattern, url, url);
     }
 
-    private boolean isFileValid(Part file, String fileName) {
+    private boolean isValidFile(Part file, String fileName) {
         // 上传的并非文件
         if (file.getContentType() == null) {
             return false;
@@ -82,17 +86,14 @@ public class FileUploadServlet extends HttpServlet {
         return String.format("%s/%s/", hash.charAt(0), hash.charAt(1));
     }
 
-    private String generateUUID() {
-        return UUID.randomUUID().toString().replace("-", "_");
-    }
 
     private String toUniqueName(String fileName) {
-        String uniqueName = generateUUID();
+        String unique = UUID.randomUUID().toString().replace("-", "_");
         int index = fileName.lastIndexOf(".");
         if (index != -1) {
-            uniqueName += fileName.substring(index);
+            unique += fileName.substring(index);
         }
-        return uniqueName;
+        return unique;
     }
 
     private String getFileName(Part part) {
@@ -110,12 +111,12 @@ public class FileUploadServlet extends HttpServlet {
         if (!dir.exists()) {
             dir.mkdirs();
 
-            makeVisitable(dir);
-            makeVisitable(dir.getParentFile());
+            setVisitable(dir);
+            setVisitable(dir.getParentFile());
         }
     }
 
-    private void makeVisitable(File dir) {
+    private void setVisitable(File dir) {
         dir.setReadable(true, false);
         dir.setExecutable(true, false);
         dir.setWritable(true, false);
